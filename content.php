df6b65099a4952cec005dc7f2921e7c7c781967e
Sdqumar <sdqumar09@gmail.com>
2021-11-15 22:32:55 +0100

    header and footer

--- content.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <h1>
-        <?php  echo 'some contnet' ?>
-    </h1>
-</div>
--- index.php
+++ index.php
@@ -1,21 +1,7 @@
-<?php
-// include('ninjas.php')
-// include 'ninjas.php'
-require 'ninjas.php'
-?>
+
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My First php</title>
-</head>
-
-<body>
-
-<?php include 'content.php' ?>
-</body>
+<?php require('templates/header.php')?>
+<?php require('templates/footer.php')?>
 
 </html>
