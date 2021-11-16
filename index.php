9c504db9e602a169cbddf67933cb4f4b4b12ef41
Sdqumar <sdqumar09@gmail.com>
2021-11-16 11:58:18 +0100

    explode function to convert sring into array

--- index.php
+++ index.php
@@ -4,20 +4,21 @@
 //MySQLi or PDO
 
 // connect to database
-$conn = mysqli_connect('localhost','test','test123','ninja_pizza');
+$conn = mysqli_connect('localhost', 'test', 'test123', 'ninja_pizza');
 
 //check connect
 if (!$conn) {
-   echo 'Connection error'. mysqli_connect_error();
+    echo 'Connection error' . mysqli_connect_error();
 };
 
 // write query for all pizzas
 
+
 // $sql = 'SELETE * FROM pizzas'
- $sql = 'SELECT id,titile,ingredients FROM `pizza`';
+$sql = 'SELECT id,titile,ingredients FROM pizza ORDER BY created_at';
 
 // make query & get result
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn, $sql);
 
 // fetch the resulting rows as an array
 
@@ -29,12 +30,41 @@ mysqli_free_result($result);
 //close connection
 mysqli_close($conn);
 
-print_r($pizzas)
+//  explode(',',$pizzas[0]['ingredients'])
+
+// print_r($pizzas)
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<?php require('templates/header.php')?>
-<?php require('templates/footer.php')?>
+<?php require('templates/header.php') ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+<div class="container">
+    <div class="row">
+        <?php foreach ($pizzas as $pizza) : ?>
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['titile']); ?></h6>
+                        <ul>
+                            <?php
+                            foreach (explode(',', $pizza['ingredients'])
+                                as $ing) : ?>
+                                <li><?php echo htmlspecialchars($ing); ?></li>
+                            <?php endforeach ?>
+                        </ul>
+                    </div>
+                        <div class="card-action right-align">
+                            <a href="#" class="brand-text">more info</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            </div>
+            
+    </div>
+
+    <?php require('templates/footer.php') ?>
 
 </html>
