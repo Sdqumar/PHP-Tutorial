2f3cb403dec716fc79b6f0cbcd084240e9d9e61e
Sdqumar <sdqumar09@gmail.com>
2021-11-16 21:52:09 +0100

    sessions

--- sandbox.php
+++ sandbox.php
@@ -8,10 +8,36 @@
 
 // $_GET['name'], $_POST['name'];
 
-echo $_SERVER['SERVER_NAME'].'<br>';
-echo $_SERVER['REQUEST_METHOD'].'<br>';
-echo $_SERVER['SCRIPT_FILENAME'].'<br>';
-echo $_SERVER['PHP_SELF'].'<br>';
-echo $_SERVER['REQUEST_METHOD'].'<br>';
+// echo $_SERVER['SERVER_NAME'].'<br>';
+// echo $_SERVER['REQUEST_METHOD'].'<br>';
+// echo $_SERVER['SCRIPT_FILENAME'].'<br>';
+// echo $_SERVER['PHP_SELF'].'<br>';
+// echo $_SERVER['REQUEST_METHOD'].'<br>';
+//sessions
+if (isset($_POST['submit'])) {
+
+session_start();
+
+$_SESSION['name'] = $_POST['name'];
+
+header('Location: index.php');
+};
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+    <input type="text" name="name">
+    <input type="submit" value="submit" name="submit">
+</form>
+</body>
+</html>
--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,11 @@
+<?php
+session_start();
 
+// if($_SERVER['QUERY_STRING'] == 'logout'){
+// unset($_SESSION['name']);
+// };
+$name = $_SESSION['name'] ?? 'Guest';
+?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -32,6 +39,7 @@
 <div class="container">
     <a href="index.php" class="brand-logo brand-text">Ninja Pizza</a>
     <ul id="nav-mobile" class="right hide-on-small-and-down">
+        <li class="grey-text">Hello <?php echo htmlspecialchars($name) ; ?></li>
         <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
     </ul>
 </div>
