fc4fbe4e120162fb68c6c3ca04290e519e9717ff
Sdqumar <sdqumar09@gmail.com>
2021-11-16 09:49:32 +0100

    connectioning to mysql using mmySQLi

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
 
+//MySQLi or PDO
+
+// connect to database
+$conn = mysqli_connect('localhost','test','test123','ninja_pizza');
+
+//check connect
+if (!$conn) {
+   echo 'Connection error'. mysqli_connect_error();
+};
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
